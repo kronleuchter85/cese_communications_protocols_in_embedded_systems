f2fdfa8439731c022a1a4b685012f3ca55a7e298
Gonzalo Carreno <kronleuchter85@gmail.com>
2022-06-15 01:23:32 +0100

    agregando estructura de carpetas  para migrar el codigo del driver desde el main al directorio aparte

---
+++ proto_stm32_display_driver/Drivers/TC1602B/inc/TC1602B.h
@@ -0,0 +1,13 @@
+/*
+ * TC1602B.h
+ *
+ *  Created on: Jun 15, 2022
+ *      Author: gonzalo
+ */
+
+#ifndef TC1602B_INC_TC1602B_H_
+#define TC1602B_INC_TC1602B_H_
+
+
+
+#endif /* TC1602B_INC_TC1602B_H_ */
---
+++ proto_stm32_display_driver/Drivers/TC1602B/src/TC1602B.c
@@ -0,0 +1,8 @@
+/*
+ * TC1602B.c
+ *
+ *  Created on: Jun 15, 2022
+ *      Author: gonzalo
+ */
+
+
